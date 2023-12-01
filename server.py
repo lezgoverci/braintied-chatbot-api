7662997424fdaaca06d6c836a9ca6fdefdc966da
Vercillius Mila Jr <vercilliusjrmila@gmail.com>
2023-12-02 01:32:42 +0800

    Initial commit

---
+++ server.py
@@ -0,0 +1,39 @@
+
+from flask import Flask, request, redirect
+from flask import jsonify
+from dotenv import load_dotenv
+from actions.openai import add_thread_message, create_run, create_thread, get_thread_last_message, retrieve_run
+
+
+
+
+from api.hubspot import hubspot
+from api.messenger import messenger
+from api.chat import chat
+import os
+
+from tools.openai import create_run_and_get_last_message
+
+load_dotenv()
+
+assistant_id = os.getenv('CHATBOT_OPENAI_ASSISTANT_ID')
+
+app = Flask(__name__)
+
+app.register_blueprint(hubspot, url_prefix='/hubspot')
+app.register_blueprint(messenger, url_prefix='/messenger')
+app.register_blueprint(chat, url_prefix='/chat')
+
+
+assistant_id = os.getenv('CHATBOT_OPENAI_ASSISTANT_ID')
+
+
+@app.route('/test', methods=['GET'])
+def test():
+    return jsonify({'message': 'GET request received!'})
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='localhost' ,debug=True, port=5002)
